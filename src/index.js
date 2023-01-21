e1509f1ac536f0840c97e30071086fec85ae703f
Mukhammadjon Tajiboev <mukhammadjon.tajiboev@gmail.com>
2023-01-21 22:19:12 +0500

    hotjar

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/main.sass'
 import 'react-toastify/dist/ReactToastify.css'
+import { hotjar } from 'react-hotjar';
 
 import TagManager from 'react-gtm-module'
+hotjar.initialize(3332378, 6)
 
 const tagManagerArgs = {
-    gtmId: 'G-MZD5DH3PTW'
+    gtmId: 'GTM-M9C6RG6'
 }
 
 TagManager.initialize(tagManagerArgs)
